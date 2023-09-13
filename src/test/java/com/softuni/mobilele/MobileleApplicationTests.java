1fb51fdf2198f4c97f3b9fc516ca021b32e16aab
Georgi133 <scrimr321@abv.bg>
2023-09-13 16:37:19 +0300

    upload

---
+++ src/test/java/com/softuni/mobilele/MobileleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.softuni.mobilele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileleApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
